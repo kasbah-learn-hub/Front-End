80027099404c0c8133862c1f48e687d049dec8ab
khabzox <abdelkabir.ouadoukou@gmail.com>
2025-07-15 22:48:14 +0100

    ✨ wrap Orb component in main layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Orb from "@/components/3d-models/orb";
 
 export default function Home() {
   return (
-    <Orb/>
+    <main className="flex h-screen flex-col items-center justify-between">
+      <Orb />
+    </main>
   );
 }
