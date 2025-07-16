0348b562153389880001962b1aaff3dd7ced0458
khabzox <abdelkabir.ouadoukou@gmail.com>
2025-07-16 21:20:35 +0100

    🗑️chore: remove SmoothScrollwrapper component and its references

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,16 @@
 import { HeroSection } from "@/components/aboutComponents/heroSection";
 import { SecendSection } from "@/components/aboutComponents/secendSection";
 
-import { SmoothScrollwrapper } from "@/components/aboutComponents/smoothScrollwrapper";
-
 export const metadata = {
   title: "about",
 };
+
 const AboutPage = () => {
   return (
-    <SmoothScrollwrapper>
+    <>
       <HeroSection />
       <SecendSection />
-    </SmoothScrollwrapper>
+    </>
   );
 };
 
