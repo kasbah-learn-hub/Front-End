670fd59efe00aafe863c9c5562ba088f07c3cf63
sofyan <soufianeboukir0@gmail.com>
2025-07-16 15:08:51 +0100

    replace o-auth(linkedin) with o-auth github

--- components/o-auth.tsx
+++ components/o-auth.tsx
@@ -19,17 +19,17 @@ export const OAuth = ({ page }: { page: string }) => {
         className="flex w-full cursor-pointer items-center justify-center gap-2 py-5"
         type="button"
       >
-        <Image src={"/icons/facebook.svg"} width={25} height={25} alt="google icon" />
+        <Image src={"/icons/facebook.svg"} width={25} height={25} alt="facebook icon" />
         {page} with Facebook
       </Button>
 
       <Button
         variant="outline"
-        className="flex w-full cursor-pointer items-center justify-center gap-2 py-5"
+        className="w-full cursor-pointer flex items-center justify-center gap-2 py-5"
         type="button"
       >
-        <Image src={"/icons/linkedin.svg"} width={20} height={20} alt="google icon" />
-        {page} with Linkedin
+        <Image src={"/icons/github.svg"} width={20} height={20} alt="github icon" className="dark:invert-0"/>
+        {page} with Github
       </Button>
     </div>
   );
