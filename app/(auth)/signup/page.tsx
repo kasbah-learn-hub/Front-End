72ab6a6326b34cdb8aa8760a3c50eff3716b4497
sofyan <soufianeboukir0@gmail.com>
2025-07-20 00:32:20 +0100

    improve SEO for login & register pages

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,5 +1,30 @@
 import { SignupForm } from "@/components/signup-form";
 import React from "react";
+import { type Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: 'Register | Kasbah Learn Hub',
+  description:
+    'Create your free account on Kasbah Learn Hub and start your journey toward accessible, engaging, and effective education today.',
+  robots: 'noindex, nofollow',
+  keywords: ['Kasbah Learn Hub', 'register', 'sign up', 'create account', 'education platform', 'online learning'],
+  openGraph: {
+    title: 'Register | Kasbah Learn Hub',
+    description: 'Join Kasbah Learn Hub and unlock access to interactive courses and global learning opportunities.',
+    // not setted yet
+    // url: 'https://kasbahlearnhub.com/register',
+    // images: [
+    //   {
+    //     url: 'https://kasbahlearnhub.com/og-image.jpg',
+    //     width: 800,
+    //     height: 600,
+    //     alt: 'Kasbah Learn Hub',
+    //   },
+    // ],
+  },
+};
+
 
 const page = () => {
   return (
